962a986d85434a9dc7b7a51d5b50aaf951bf5565
lucastb <lucas.t.b@msn.com>
2016-10-20 19:53:14 -0200

    Initial commit
    Change-Id: I0300744a6358d59e7d3fdbda03e567b53ef0ee76
    Signed-off-by: Lucas Barros <lucas.t.b@msn.com>

---
+++ e06/src/main/java/fds/e06/fds/e06/App.java
@@ -0,0 +1,13 @@
+package fds.e06.fds.e06;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!!!" );
+    }
+}
